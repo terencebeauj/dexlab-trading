5060869656d159f322ec9c5bbbed9c488c85e397
Terence <beaujour.t@hotmail.fr>
2021-11-08 13:28:54 -0500

    update file

--- tests/get_market_price_by_address.py
+++ tests/get_market_price_by_address.py
@@ -3,5 +3,5 @@ from connector.dexlab import *
 address = "8xhfMZpJTvuYBqzmKM3jQkzk3gcT8Pz7AnuxNUeLo6mY"
 
 dexlab = Dexlab()
-mp = dexlab.get_market_price(address)
+mp = dexlab.get_market_price_by_address(address)
 print(mp)
